c8922019c22484044531392914cff66720cc0cd0
Amiir25 <amirsadik636@gmail.com>
2025-07-19 20:04:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 import express from 'express';
 import { PORT } from "./config/env.js";
+import cookieParser from 'cookie-parser';
 
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subsRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from './middlewares/error.middleware.js';
 
 // Initializing our application
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false})); // Helps to process the form data sent via HTML forms in a simple format
+app.use(cookieParser()) // Reads cookies fom incoming requests to store user data
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subsRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription tracker API');
