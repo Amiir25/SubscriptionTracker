c131f8adb10891a6b6eae89098d6636daa42d81e
Amiir25 <amirsadik636@gmail.com>
2025-07-19 14:47:21 +0300

    Add subscription.model.js

---
+++ models/subscription.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const subsSchema = new mongoose.Schema({
+    name: {
+        type: true,
+        required: [true, 'Subscription name is required'],
+        trim: true,
+        minLength: 2,
+        maxLength: 100,
+    },
+    price: {
+        type: Number,
+        required: [true, 'Subscription price is required'],
+        min: [0, 'Price must be greater than 0'],
+    },
+    currency: {
+        type: String,
+        enum: ['USD', 'EUR', 'GBP'],
+        default: 'USD',
+    },
+    frequency: {
+        type: String,
+        enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    },
+    catagory: {
+        type: String,
+        enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'other'],
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'cancelled', 'expired'],
+        default: 'active',
+    }
+    
+
+}, {timestamps: true});
