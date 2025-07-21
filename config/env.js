850189e559fe20e1c58635638abad444e836c718
Amiir25 <amirsadik636@gmail.com>
2025-07-21 22:22:09 +0300

    Update env.js

--- config/env.js
+++ config/env.js
@@ -3,8 +3,9 @@ import { config } from 'dotenv';
 config({path: `.env.${process.env.NODE.ENV || 'development'}.local`});
 
 export const {
-    PORT, NODE_ENV,
+    PORT, SERVER_URL, NODE_ENV,
     DB_URI,
     JWT_SECRET, JWT_EXPIRES_IN,
     ARKJET_KEY, ARKJET_ENV,
+    QSTASH_TOKEN, QSTASH_URL
 } = process.env;
